84a9be21b1d0b6f701cbc5c17dd8cb70121d6a44
igorzg <igor.zg1987@gmail.com>
2018-01-10 22:50:49 +0100

    Update of status spec

--- test/controllers/StatusControllerSpec.scala
+++ test/controllers/StatusControllerSpec.scala
@@ -2,6 +2,7 @@ package controllers
 
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
+import play.api.{Configuration, Environment}
 import play.api.test._
 import play.api.test.Helpers._
 
@@ -11,12 +12,12 @@ import play.api.test.Helpers._
  *
  * For more information, see https://www.playframework.com/documentation/latest/ScalaTestingWithScalaTest
  */
-class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
+class StatusControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
 
   "HomeController GET" should {
 
     "render the index page from a new instance of controller" in {
-      val controller = new HomeController(stubControllerComponents())
+      val controller = new StatusController(stubControllerComponents(), Configuration.load(Environment.simple()))
       val home = controller.index().apply(FakeRequest(GET, "/"))
 
       status(home) mustBe OK
@@ -25,7 +26,7 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
     }
 
     "render the index page from the application" in {
-      val controller = inject[HomeController]
+      val controller = inject[StatusController]
       val home = controller.index().apply(FakeRequest(GET, "/"))
 
       status(home) mustBe OK
