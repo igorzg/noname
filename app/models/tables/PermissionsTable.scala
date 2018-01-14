df7a578ae8317cc008c6096de5d37008b405ef51
igorzg <igor.zg1987@gmail.com>
2018-01-14 13:49:47 +0100

    Added permisons & role table

---
+++ app/models/tables/PermissionsTable.scala
@@ -0,0 +1,27 @@
+package models.tables
+
+import models.entity.{Permission}
+import play.api.db.slick.HasDatabaseConfig
+import slick.jdbc.JdbcProfile
+
+/**
+  * @author igorzg on 14.01.18.
+  * @since 1.0
+  */
+trait PermissionsTable {
+  self: HasDatabaseConfig[JdbcProfile] =>
+
+  import profile.api._
+
+  class PermissionsTable(tag: Tag) extends Table[Permission](tag, "Permissions") {
+
+    def perm_id = column[Option[Int]]("perm_id", O.PrimaryKey, O.AutoInc)
+
+    def name = column[String]("name")
+
+    def action = column[String]("action")
+
+    def * = (perm_id, name, action) <> ((Permission.apply _).tupled, Permission.unapply)
+  }
+
+}
---
+++ app/models/tables/RolesTable.scala
@@ -0,0 +1,25 @@
+package models.tables
+
+import models.entity.Role
+import play.api.db.slick.HasDatabaseConfig
+import slick.jdbc.JdbcProfile
+
+/**
+  * @author igorzg on 14.01.18.
+  * @since 1.0
+  */
+trait RolesTable {
+  self: HasDatabaseConfig[JdbcProfile] =>
+
+  import profile.api._
+
+  class RolesTable(tag: Tag) extends Table[Role](tag, "Roles") {
+
+    def role_id = column[Option[Int]]("role_id", O.PrimaryKey, O.AutoInc)
+
+    def name = column[String]("name")
+
+    def * = (role_id, name) <> ((Role.apply _).tupled, Role.unapply)
+  }
+
+}
