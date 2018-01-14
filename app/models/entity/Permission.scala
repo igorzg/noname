6f7bdee221cb951c727503229d5eba6fd6937669
igorzg <igor.zg1987@gmail.com>
2018-01-14 13:35:21 +0100

    Added Permission class

---
+++ app/models/entity/Permission.scala
@@ -0,0 +1,7 @@
+package models.entity
+
+/**
+  * @author igorzg on 14.01.18.
+  * @since 1.0
+  */
+case class Permission(perm_id: Option[Int], name: String, action: String)
--- app/models/entity/Role.scala
+++ app/models/entity/Role.scala
@@ -4,4 +4,4 @@ package models.entity
   * @author igorzg on 14.01.18.
   * @since 1.0
   */
-case class Roles(role_id: Option[Int], name: String)
+case class Role(role_id: Option[Int], name: String)
