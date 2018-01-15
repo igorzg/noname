50959199a8411685948d62b9d65f62323c7fbc0b
igorzg <igor.zg1987@gmail.com>
2018-01-15 22:12:50 +0100

    Update of controller and added credentials

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -3,18 +3,15 @@ package controllers
 import java.util.UUID
 import javax.inject.{Inject, Singleton}
 
+import models.Credentials
 import models.dao.UsersDao
 import models.entity.User
-import org.json4s.JsonAST.JString
 import org.json4s.{DefaultFormats, FieldSerializer, Formats}
 import org.json4s.native.Serialization.{write, writePretty}
 import org.json4s.native.parseJsonOpt
-import play.api.mvc.{AbstractController, ControllerComponents}
+import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
 
 import scala.concurrent.{ExecutionContext, Future}
-
-
-case class Credentials(username: String, password: String)
 
 /**
   * This controller creates an `Action` to handle HTTP requests to the
@@ -36,14 +33,14 @@ class UserController @Inject()(cc: ControllerComponents, userDao: UsersDao)(impl
   implicit val formats: Formats = DefaultFormats
 
 
-  def index() = Action.async { implicit request =>
+  def index(): Action[AnyContent] = Action.async { implicit request =>
     userDao.all().map {
       users => Ok(writePretty(users)(formats + ignoreFields))
     }
   }
 
 
-  def authenticate() = Action.async { implicit request =>
+  def authenticate(): Action[AnyContent] = Action.async { implicit request =>
     val data = parseJsonOpt(request.body.asText.get)
     if (data.isDefined) {
       val credentials: Option[Credentials] = data.get.extractOpt[Credentials]
@@ -51,33 +48,29 @@ class UserController @Inject()(cc: ControllerComponents, userDao: UsersDao)(impl
         userDao.verifyUser(credentials.get.username, credentials.get.password).map {
           isLoggedIn: Boolean => {
             if (isLoggedIn) {
-              Ok(
-                write(
-                  Map(
-                    "message" -> "User successfully authenticated",
-                    "token" -> UUID.randomUUID().toString.concat("-" + System.currentTimeMillis().toString)
-                  )
+              Ok(write(
+                Map(
+                  "message" -> "User successfully authenticated",
+                  "token" -> UUID.randomUUID().toString.concat("-" + System.currentTimeMillis().toString)
                 )
-              )
+              ))
             } else Unauthorized
-
           }
         }
-      } else Future.successful(
-        BadRequest(
-          write(
-            "message" -> JString("Invalid parameters {username, password}")
+      } else {
+        Future.successful(BadRequest(write(
+          Map(
+            "message" -> "Invalid parameters {username, password}"
           )
+        )))
+      }
+    } else {
+      Future.successful(BadRequest(write(
+        Map(
+          "message" -> "Authenticate parameters has to be provided {username, password}"
         )
-      )
-    } else
-      Future.successful(
-        BadRequest(
-          write(
-            "message" -> JString("Authenticate parameters has to be provided {username, password}")
-          )
-        )
-      )
+      )))
+    }
   }
 
 }
---
+++ app/models/Credentials.scala
@@ -0,0 +1,8 @@
+package models
+
+/**
+  * @author igorzg on 15.01.18.
+  * @since 1.0
+  */
+
+case class Credentials(username: String, password: String)
